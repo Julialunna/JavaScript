3377c54f1f51a88b30f2e8959a5c6bf1c7e58a6e
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2022-12-06 17:05:46 -0300

    fazendo main do aluramidi e desafio

--- Alura/aluramidi/main.js
+++ Alura/aluramidi/main.js
@@ -1,9 +1,35 @@
 //som pom
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(seletorAudio){
+
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento &&elemento.localName==='audio'){
+         elemento.play(); 
+    }else{
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+for(let contador =0;contador < listaDeTeclas.length; contador++){
 
-listaDeTeclas[0].onclick=tocaSomPom;
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    
+    //console.log(idAudio);
+
+    tecla.onclick= function(){
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(event){
+        if(event.code==="Space"||event.code==="Enter"){
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+}
--- Alura/desafios/d001/desafio.js
+++ Alura/desafios/d001/desafio.js
@@ -1,14 +1,26 @@
-document.querySelector('input[type="button"] ')
 const listaDeTeclas =  document.querySelectorAll('input[type=button]');
-console.log(listaDeTeclas[0].value);
-console.log(listaDeTeclas[1].value);
-console.log(listaDeTeclas[2].value);
-console.log(listaDeTeclas[3].value);
-console.log(listaDeTeclas[4].value);
-console.log(listaDeTeclas[5].value);
-console.log(listaDeTeclas[6].value);
-console.log(listaDeTeclas[7].value);
-console.log(listaDeTeclas[8].value);
-console.log(listaDeTeclas[9].value);
-console.log(listaDeTeclas[10].value);
-console.log(listaDeTeclas[11].value);
+
+function imprimeNoCampo(valueTecla){
+    //const informcao = document.querySelector('input[type=tel]').value
+    document.querySelector('input[type=tel]').value=valueTecla;
+    //informcao=informcao+valueTecla;
+    //window.print(valueTecla);
+}
+
+
+for(let i=0; i<listaDeTeclas.length;i++){
+    const tecla = listaDeTeclas[i];
+    const valor= tecla.value;
+
+    tecla.onclick=function(){
+        imprimeNoCampo(valor);
+    }
+    tecla.onkeydown=function(){
+        tecla.classList.add('ativa');
+    }
+    tecla.onkeyup=function(){
+        tecla.classList.remove('ativa');
+    }
+    
+}
+
