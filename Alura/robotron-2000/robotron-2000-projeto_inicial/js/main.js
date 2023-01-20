5e86675f592128d57073fe5388a656f55af34462
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-01-20 19:44:05 -0300

    fazendo desafio + anotações

--- Alura/robotron-2000/robotron-2000-projeto_inicial/js/main.js
+++ Alura/robotron-2000/robotron-2000-projeto_inicial/js/main.js
@@ -1,12 +1,8 @@
-const subtrair = document.querySelector('#subtrair');
-const somar = document.querySelector('#somar');
-
-
-const controle = document.querySelectorAll(".controle-ajuste");
+const controle = document.querySelectorAll("[data-controle]");
 
 controle.forEach((elemento)=>{
     elemento.addEventListener("click",(evento)=>{
-        manipulaDados(evento.target.textContent, evento.target.parentNode);
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
         console.log(evento.target.parentNode);
     } )
 })
@@ -16,7 +12,7 @@ controle.forEach((elemento)=>{
 
 
 function manipulaDados(operacao, controle){
-    const peca = controle.querySelector('.controle-contador');
+    const peca = controle.querySelector('[data-contador]');
     if(operacao === "-"){
         peca.value= parseInt(peca.value)-1;  
     }else{
