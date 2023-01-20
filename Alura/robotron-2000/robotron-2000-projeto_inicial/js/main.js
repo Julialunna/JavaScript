ce105c537d9884327725eced5d49560cf132bf3d
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-01-19 21:05:51 -0300

    aprendendo sobre arrays

--- Alura/robotron-2000/robotron-2000-projeto_inicial/js/main.js
+++ Alura/robotron-2000/robotron-2000-projeto_inicial/js/main.js
@@ -1,12 +1,23 @@
-const robotron = document.querySelector('#robotron');
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+const braco = document.querySelector('#braco');
 
-robotron.addEventListener("click", (evento)=>{
-    console.log(evento);    
-});
+const controle = document.querySelectorAll(".controle-ajuste");
 
-function dizOi (nome){
-    console.log("oi " + nome);
-    console.log("bem-vindo ao robotron 2000");
+controle.forEach((elemento)=>{
+    elemento.addEventListener("click",(evento)=>{
+        manipulaDados(evento.target.textContent);
+    } )
+})
+
+//somar.addEventListener("click", ()=>{manipulaDados("somar")});
+//subtrair.addEventListener("click", ()=>{manipulaDados("subtrair")});
+
+
+function manipulaDados(operacao){
+    if(operacao === "-"){
+        braco.value= parseInt(braco.value)-1;  
+    }else{
+        braco.value= parseInt(braco.value)+1;  
+    }
 }
-
-dizOi("julia");
