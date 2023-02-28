67c26c059c1ff673c0f882431d871c580af90480
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-02-28 16:05:54 -0300

    teste

---
+++ type-string.js
@@ -0,0 +1,11 @@
+const texto1 ="Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha ="SenhaSuperSegura123!";
+const stringDeNumeros = "34567";
+
+const citação = 'Meu nome é';
+const meuNome = " Leonardo";
+
+//concatenação (+)
+
+console.log(citação+meuNome);
