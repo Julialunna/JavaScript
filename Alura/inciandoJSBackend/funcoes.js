dbe4ca8428d063a98a03baea53c288e4e3d24898
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-03-03 12:37:03 -0300

    aprendendo funções

---
+++ Alura/inciandoJSBackend/funcoes.js
@@ -0,0 +1,23 @@
+/*let x ="";
+console.log(x);
+x="oi";*/
+
+//declaração de função
+
+function imprimeTexto(texto){
+    console.log(texto);
+}
+//chamando função
+
+imprimeTexto("oi");
+imprimeTexto("outro texto");
+
+//Três formas de escrever funções
+
+function soma(){
+    return resultado = 2+2;
+}
+
+imprimeTexto(soma());
+
+//console.log(soma());
