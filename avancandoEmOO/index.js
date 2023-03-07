cecdea8180b3fd19ebd05249b28f1defb4a4107c
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-03-07 16:58:41 -0300

    fazendo getters

--- avancandoEmOO/index.js
+++ avancandoEmOO/index.js
@@ -6,4 +6,4 @@ import Docente from "./Docente.js"
 const novoUser= new User('Mariana', "m@m.com", "2021-01-01");
 console.log(novoUser.exibirInfos())
 const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2021-01-01');
-console.log(novoAdmin.exibirInfos())
+console.log(novoAdmin.nome)
