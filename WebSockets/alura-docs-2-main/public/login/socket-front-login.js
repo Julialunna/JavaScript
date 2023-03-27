9825506bced0714f805c73d312dcd30664a25bc7
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-03-27 15:45:55 -0300

    guardando jwt em cookies[

--- WebSockets/alura-docs-2-main/public/login/socket-front-login.js
+++ WebSockets/alura-docs-2-main/public/login/socket-front-login.js
@@ -1,9 +1,11 @@
+import { definirCookie } from "../utils/cookies.js";
 
 const socket=io();
 function emitirAutenticarUsuario(dados){
     socket.emit("autenticar_usuario", dados);
 }
-socket.on("autenticação_sucesso", ()=>{
+socket.on("autenticação_sucesso", (tokenJwt)=>{
+    definirCookie("tokenJwt", tokenJwt);
     alert("Usuário autenticado com sucesso =)");
     window.location.href="/";
 });
